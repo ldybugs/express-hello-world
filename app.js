c81f11d49dece6f9f1fec37398b968d40ebac16a
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2023-09-21 13:46:46 -0400

    commit meunome

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+  console.log("Just got a request!")
+  res.send('Vitória de Oliveira Medeiros')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
