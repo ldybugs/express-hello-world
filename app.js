1c24a01dc9acaec5bd6d08fabd58b8e6ba3df1c8
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2023-09-21 14:52:59 -0400

    teste pokemons

--- app.js
+++ app.js
@@ -25,11 +25,6 @@ app.get('/tico', (req, res) => {
 var pokemons = ['Pikachu', 'Butterfree', 'Pidgeot', 'Charizard', 'Bulbasaur', 'Squirtle', 'Kingler', 'Tauros', 'Muk', 'Primeape']
 
 app.get('/pokemons', (req, res) => {
-  console.log("Just got a request!")
-  res.send('')
-})
-
-app.get('/pokemons', (req, res) => {
   let id = req.params.id;
   return res.json([pokemons])
 });
