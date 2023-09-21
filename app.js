a15ae0ca38c481f9714f61da9bf6b71147960653
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2023-09-21 14:58:14 -0400

    commit series

--- app.js
+++ app.js
@@ -29,6 +29,19 @@ app.get('/pokemons', (req, res) => {
   return res.json([pokemons])
 });
 
+var series = ['The Umbrella Academy', 'Outer Banks', 'Pretendente Surpresa'];
+
+app.post('/series/', (req, res) => {
+  let name = req.body.name;
+  series[(series.length)] = name;
+  return res.json([series[(series.length - 1)]]);
+});
+
+app.get('/series', (req, res) => {
+  let id = req.params.id;
+  return res.json([series])
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
